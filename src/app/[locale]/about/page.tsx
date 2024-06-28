13809a936810c93a9fa1921fe3960a25b1acf122
Raul Marin <raulm0429@icloud.com>
2024-06-28 07:19:13 -0500

    test commit for webhook

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow px-32 py-24 text-center text-2xl">
         {t(
-          'This_is_a_simple_about_page_just_to_show_how_you_can_use_the_test_different_pages'
+          'This_is_a_simple_about_page_just_to_show_how_you_can_use_test_different_pages'
         )}
       </main>
       <footer>
